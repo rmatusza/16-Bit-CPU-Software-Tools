0e0c3de22ce188b8c1ea79332ce56d08115a1214
ryan <ryan.d.matuszak@gmail.com>
2026-04-29 18:14:15 -0500

    changing branch

--- VM Translator/src/Translator.java
+++ VM Translator/src/Translator.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 
 public class Translator {
     public static void main(String[] args) throws IOException {
-        new CodeWriter("C:\\Program Files\\nand2tetris\\projects\\12\\ScreenTest");
+        new CodeWriter("VM Translator/test");
     }
 }
