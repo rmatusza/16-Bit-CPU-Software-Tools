21398bc766d4454d96821cf1032c1efea5def788
ryan <ryan.d.matuszak@gmail.com>
2026-05-20 17:18:56 -0500

    started refactoring translator util to eliminate repetition

--- VM Translator/src/util/TranslatorUtil.java
+++ VM Translator/src/util/TranslatorUtil.java
@@ -8,7 +8,8 @@ import java.util.Map;
 public class TranslatorUtil {
     private final StringBuilder sb = new StringBuilder();
     private int id = 0;
-    private final Map<String, String> functionNames = new HashMap<>();
+    private final List<String> returnPointers = List.of("THAT", "THIS", "ARG", "LCL");
+    private final List<String> callPointers = List.of("LCL", "ARG", "THIS", "THAT");
     private final Map<String, String> operations = new HashMap<>(Map.of(
             "add", "+",
             "sub", "-",
@@ -33,14 +34,132 @@ public class TranslatorUtil {
         id++;
     }
 
-    public void saveOffsetAddressToTemp(String address, String offset, String temp) {
-        sb.append("@").append(address).append(" ")
-                .append("D=M ")
-                .append("@").append(offset).append(" ")
-                .append("D=D+A ")
-                .append("@").append(temp).append(" ")
+    /*                     UTIL API                                                 */
+
+    public void setA(String val){
+        sb.append("@").append(val).append(" ");
+    }
+
+    // write D to pointer address
+    public void writeDToMAMA(String address) {
+        setA(address);
+        sb.append("A=M ")
                 .append("M=D ");
     }
+
+    // write D to variable address
+    public void writeDToMA(String address) {
+        setA(address);
+        sb.append("M=D ");
+    }
+
+    public void writeConstToMA(String c) {
+        sb.append("M=").append(c).append(" ");
+    }
+
+    // write pointer address to D
+    public void writeMAMAToD(String address) {
+        sb.append("@").append(address).append(" ")
+                .append("A=M ")
+                .append("D=M ");
+    }
+
+    // write variable address to D
+    public void writeMAToD(String address) {
+        setA(address);
+        sb.append("D=M ");
+    }
+
+    // write constant or label to D
+    public void writeAToD(String i) {
+        setA(i);
+        sb.append("D=A ");
+    }
+
+    public void writeMAMAOpDToD(String a, String op) {
+        setA(a);
+        sb.append("A=M").append(op).append("D ")
+                .append("D=M ");
+    }
+
+    public void writeMAOpConstToD(String a, String op, int c) {
+        setA(a);
+        sb.append("D=M").append(op).append(c).append(" ");
+    }
+
+    public void writeMAToA(String i) {
+        setA(i);
+        sb.append("A=M ");
+    }
+
+    // D +/- A
+    public void offsetDByA(int offset, String op) {
+        setA(offset+"");
+        sb.append("D=D").append(op).append("A").append(" ");
+    }
+
+    public void offsetAByMAOpD(String addressBase, String op) {
+        setA(addressBase);
+        sb.append("A=M").append(op).append("D").append(" ");
+    }
+
+    // push D to stack and increment
+    public void pushD() {
+        setA("SP");
+        sb.append("A=M ")
+                .append("M=D ")
+                .append("@SP ")
+                .append("M=M+1 ");
+    }
+
+    // pop top of stack to D
+    public void popToD() {
+        setA("SP");
+        sb.append("M=M-1 ")
+                .append("A=M ")
+                .append("D=M ");
+    }
+
+    // get top of stack address
+    public void getNextStackAddress() {
+        setA("SP");
+        sb.append("M=M-1 ")
+                .append("A=M ");
+    }
+
+    public void writeLabelRef(String name, String label){
+        sb.append("@").append(name).append(".").append(label).append(" ");
+    }
+
+    public void writeLabel(String name, String label){
+        sb.append("(").append(name).append(".").append(label).append(") ");
+    }
+
+    public void writeGoto(String name, String label){
+        writeLabelRef(name, label);
+        jump();
+    }
+
+    public void writeGotoVarAddress(String v) {
+        writeMAToA(v);
+        jump();
+    }
+
+    public StringBuilder generateReturnAddressName(String name) {
+        StringBuilder b = new StringBuilder();
+        b.append(name).append("_").append("RETURN").append("_").append(id++);
+        return b;
+    }
+
+    public void jump() {
+        sb.append("0;JMP").append(" ");
+    }
+
+    public void jump(String cond){
+        sb.append("D;").append(cond).append(" ");
+    }
+
+    /*                          CODE WRITER API                                            */
 
     public void writeDToM(String address) {
         sb.append("@").append(address).append(" ")
@@ -56,6 +175,15 @@ public class TranslatorUtil {
     public void incrementSP() {
         sb.append("@SP ")
                 .append("M=M+1 ");
+    }
+
+    public void saveOffsetAddressToTemp(String address, String offset, String temp) {
+        sb.append("@").append(address).append(" ")
+                .append("D=M ")
+                .append("@").append(offset).append(" ")
+                .append("D=D+A ")
+                .append("@").append(temp).append(" ")
+                .append("M=D ");
     }
 
     public void pop(String address, String offset){
@@ -111,24 +239,6 @@ public class TranslatorUtil {
                     .append("M=D ");
             incrementSP();
         }
-    }
-
-    public void pushLabel(String label){
-        sb.append("@").append(label).append(" ")
-                .append("D=A ")
-                .append("@SP ")
-                .append("A=M ")
-                .append("M=D ");
-        incrementSP();
-    }
-
-    public void pushPointer(String pointer){
-        sb.append("@").append(pointer).append(" ")
-                .append("D=M ") // D = address that the pointer is pointing to
-                .append("@SP ")
-                .append("A=M ")
-                .append("M=D ");
-        incrementSP();
     }
 
     public void binaryOp(String operation) {
@@ -187,14 +297,6 @@ public class TranslatorUtil {
 
     // PROGRAM CONTROL
 
-    private void writeLabelRef(String name, String label){
-        sb.append("@").append(name).append(".").append(label).append(" ");
-    }
-
-    public void writeLabel(String filename, String label){
-        sb.append("(").append(filename).append(".").append(label).append(") ");
-    }
-
     private void writeLabelRef(String name){
         sb.append("@").append(name).append(" ");
     }
@@ -203,112 +305,75 @@ public class TranslatorUtil {
         sb.append("(").append(name).append(") ");
     }
 
-    public StringBuilder generateReturnAddressName(String name) {
-        StringBuilder b = new StringBuilder();
-        b.append(name).append("_").append("RETURN").append("_").append(id++);
-        return b;
-    }
-
     public void pushReturnAddress(String returnAddr) {
-        pushLabel(returnAddr);
-    }
-
-    public void writeGoto(String name, String label){
-        writeLabelRef(name, label);
-        sb.append("0;JMP ");
+        writeAToD(returnAddr);
+        pushD();
     }
 
     public void writeGoto(String name){
         writeLabelRef(name);
-        sb.append("0;JMP ");
-    }
-
-    public void writeGotoVarAddress(String v) {
-        sb.append("@").append(v).append(" ")
-                .append("A=M ")
-                .append("0;JMP ");
+        jump();
     }
 
     public void writeIf(String filename, String label){
-        pop();
+        popToD();
         writeLabelRef(filename, label);
-        sb.append("D;JNE ");
+        jump("JNE");
     }
 
     public void pushCallerPointers() {
-        pushPointer("LCL");
-        pushPointer("ARG");
-        pushPointer("THIS");
-        pushPointer("THAT");
+        for(var p : callPointers) {
+            writeMAToD(p);
+            pushD();
+        }
     }
 
     public void repositionCalleeArg(int numArgs) {
         int i = numArgs + 5;
-        sb.append("@SP ")
-                .append("D=M ")
-                .append("@").append(i).append(" ")
-                .append("D=D-A ")
-                .append("@ARG ")
-                .append("M=D ");
+        writeMAToD("SP");
+        offsetDByA(i, operations.get("sub"));
+        writeDToMA("ARG");
     }
 
     public void repositionCalleeLcl(){
-        sb.append("@SP ")
-                .append("D=M ")
-                .append("@LCL ")
-                .append("M=D ");
+        writeMAToD("SP");
+        writeDToMA("LCL");
     }
 
     public void setFrameVar() {
-        sb.append("@LCL ")
-                .append("D=M ")
-                .append("@FRAME ")
-                .append("M=D ");
-    }
-
-    private void modifyPointerViaFrame(int i, String pointer) {
-        sb.append("@").append(i).append(" ")
-                .append("D=A ")
-                .append("@FRAME ")
-                .append("A=M-D ") // A = address that contains the pointer address
-                .append("D=M ") // D = pointer address
-                .append("@").append(pointer).append(" ")
-                .append("M=D ");
+        writeMAToD("LCL");
+        writeDToMA("FRAME");
     }
 
     public void setReturnAddrVar(){
-        sb.append("@").append(5).append(" ")
-                .append("D=A ")
-                .append("@FRAME ")
-                .append("A=M-D ") // A = address that contains the return address
-                .append("D=M ") // D = return address
-                .append("@RET ")
-                .append("M=D ");
+        writeAToD("5");
+        writeMAMAOpDToD("FRAME", operations.get("sub"));
+        writeDToMA("RET");
     }
 
     public void setReturnValue(){
-        pop();
-        writeDToMViaPointer("ARG");
+        popToD();
+        writeDToMAMA("ARG");
     }
 
     public void restoreCallerSP(){
-        sb.append("@").append("ARG ")
-                .append("D=M+1 ")
-                .append("@SP ")
-                .append("M=D ");
+        writeMAOpConstToD("ARG", operations.get("add"), 1);
+        writeDToMA("SP");
     }
 
     public void restoreCallerPointers(){
-        List<String> pointers = List.of("THAT", "THIS", "ARG", "LCL");
         for (int i = 0; i < 4; i++) {
-            modifyPointerViaFrame(i+1, pointers.get(i));
+            writeAToD(i+1+"");
+            writeMAMAOpDToD("FRAME", operations.get("sub"));
+            writeDToMA(returnPointers.get(i));
         }
     }
 
     public void initializeLocals(int numLocals){
         for(int i=0; i<numLocals; i++){
-            push(i+"", "");
-            pop("LCL", i+"");
+           writeAToD(i+"");
+           offsetAByMAOpD("LCL", operations.get("add"));
+           writeConstToMA("0");
         }
     }
 }
